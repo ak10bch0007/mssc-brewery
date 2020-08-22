966eac115c92746ef2ba6e2ed0d23d14800dfbfc
Ashok <ak10bch0007@gmail.com>
2020-08-22 19:31:54 +0530

    Lecture 37

---
+++ src/main/java/guru/springframework/msscbrewery/services/v2/BeerServiceV2Impl.java
@@ -0,0 +1,30 @@
+package guru.springframework.msscbrewery.services.v2;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BeerServiceV2Impl implements BeerServiceV2 {
+    @Override
+    public BeerDtoV2 getBeerById(UUID beerId) {
+        return null;
+    }
+
+    @Override
+    public BeerDtoV2 saveNewBeer(BeerDto beerDto) {
+        return null;
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+
+    }
+}
